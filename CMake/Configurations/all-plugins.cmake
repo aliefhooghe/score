c0f1bcec191c8815d469fa87b7044938852accf6
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-11-24 15:13:28 +0100

    remove stuff for segment

--- CMake/Configurations/all-plugins.cmake
+++ CMake/Configurations/all-plugins.cmake
@@ -2,6 +2,4 @@ include(default-plugins)
 set(ISCORE_PLUGINS_TO_BUILD
   ${ISCORE_PLUGINS_TO_BUILD}
 
-iscore-plugin-library
-
 )
--- CMake/Configurations/default-plugins.cmake
+++ CMake/Configurations/default-plugins.cmake
@@ -1,22 +1,12 @@
 set(ISCORE_PLUGINS_TO_BUILD
 
-iscore-plugin-pluginsettings
 iscore-lib-inspector
 iscore-lib-state
 iscore-lib-process
 iscore-lib-device
 
-iscore-plugin-inspector
 iscore-plugin-deviceexplorer
 iscore-plugin-curve
 iscore-plugin-automation
 iscore-plugin-scenario
-iscore-plugin-loop
-
-iscore-plugin-mapping
-iscore-plugin-engine
-iscore-plugin-js
-iscore-plugin-midi
-iscore-plugin-recording
-iscore-plugin-media
 )
